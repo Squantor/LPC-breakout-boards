8ca3c6a6e93b32ccacabb5848b1af3a41f153e1d
Bart Bilos <squantmuts@gmail.com>
2019-03-31 19:46:42 +0200

    updated makefiles to support multiple boards

--- software/breakout_tester/src/Makefile.mk
+++ software/breakout_tester/src/Makefile.mk
@@ -1,53 +1,25 @@
-# update per change V0004
+# update per change V0006
 
 # project name
 BIN_NAME = breakout_tester
 BOARD = lpc82xJDH20_breakout
 
-#include board specific settings
+#include board specific settings/rules
 include src/$(BOARD).mk
 
 # project settings
 C_SOURCES +=
 CXX_SOURCES += src/startup.cpp src/main.cpp src/gpio_tester.cpp src/ticks.cpp
 S_SOURCES += src/aeabi_romdiv_patch.s
-INCLUDES += -Iinc -I../lpc_chip_82x/inc
-ALIBS += -llpc_chip_82x -lsqlibc
+INCLUDES += -Iinc
+DEFINES +=
+ALIBS +=
 RLIBS +=
 DLIBS +=
 ALIBDIR +=
 RLIBDIR += -L"../lpc_chip_82x/bin/release" -L"../squantorLibC/bin/CortexM0/release"
 DLIBDIR += -L"../lpc_chip_82x/bin/debug" -L"../squantorLibC/bin/CortexM0/debug"
-DEFINES += -DCORE_M0PLUS -D__USE_ROMDIVIDE -DCHIP_LPC82X
 COMPILE_C_FLAGS = -std=gnu11 -Wall -Wextra -Wno-main -fno-common -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections
 COMPILE_CXX_FLAGS = -std=c++17 -Wall -Wextra -Wno-main -fno-common -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fno-rtti -fno-exceptions
 COMPILE_ASM_FLAGS = -c -x assembler-with-cpp
 LINK_FLAGS +=  -nostdlib -Wl,--gc-sections -Wl,-print-memory-usage
-
-#custom build rules
-pre-clean:
-	$(MAKE) -C ../lpc_chip_82x clean MCU=LPC82X
-	$(MAKE) -C ../squantorLibC clean
-
-pre-release:
-	$(MAKE) -C ../lpc_chip_82x release MCU=LPC82X
-	$(MAKE) -C ../squantorLibC release PLATFORM=CortexM0
-
-pre-debug:
-	$(MAKE) -C ../lpc_chip_82x debug MCU=LPC82X
-	$(MAKE) -C ../squantorLibC debug PLATFORM=CortexM0
-
-#project hardware specific commands
-gdbusbdebug: debug
-	$(TOOLCHAIN_PREFIX)$(GDB) -x ./gdb_scripts/bmpUSBdebug.txt
-
-gdbusbrelease: release
-	$(TOOLCHAIN_PREFIX)$(GDB) -x ./gdb_scripts/bmpUSBrelease.txt
-
-tpwrdisable:
-	$(TOOLCHAIN_PREFIX)$(GDB) -x ./gdb_scripts/bmpusb_tpwr_disable.txt
-
-tpwrenable:
-	$(TOOLCHAIN_PREFIX)$(GDB) -x ./gdb_scripts/bmpusb_tpwr_enable.txt
-
-.PHONY: gdbftdidebug gdbftdirelease gdbusbdebug gdbusbrelease tpwrdisable tpwrenable
