8fc043bb9b4caf62227b3d99d49acf668c0696e2
Bart Bilos <squantmuts@gmail.com>
2019-04-26 11:47:03 +0200

    cleanup of project

--- software/lpc824_adc_eval/src/startup.cpp
+++ software/lpc824_adc_eval/src/startup.cpp
@@ -1,13 +1,35 @@
 /*
- * Unified Cortex Startup - C++ startup file
- *
- * This file is in public domain
- *
- * Put together by Paul Sokolovsky based on article by Vanya Sergeev
- * http://dev.frozeneskimo.com/notes/cortex_cmsis/ , GNU ld documentation
- * and numerous other public resources.
- *
- */
+MIT License
+
+Copyright (c) 2019 Bart Bilos
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+/*
+Unified Cortex Startup - C++ startup file
+
+Put together by Paul Sokolovsky based on article by Vanya Sergeev
+http://dev.frozeneskimo.com/notes/cortex_cmsis/ , GNU ld documentation
+and numerous other public resources.
+modified and extended for use with LPC824 by Bart Bilos
+
+*/
 
 #include <stdint.h>
 
